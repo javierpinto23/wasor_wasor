5553ac461a9591e91bea780e368d7293add5ee04
PintoJav <javier.pinto@adidas.com>
2024-02-26 17:16:51 -0500

    tuning the model with two epochs

--- model_train.py
+++ model_train.py
@@ -5,4 +5,4 @@ import torch
 model = YOLO("model/wasor_model.pt")
 
 # Train the model
-results = model.train(data="/Users/pintojav/Desktop/wasor_wasor/wasor_wasor/wasor_data.yaml", epochs=1, imgsz=640, device="mps", project="wasor_wasor",name="runs")
+results = model.train(data="/Users/pintojav/Desktop/wasor_wasor/wasor_wasor/wasor_data.yaml", epochs=2, imgsz=640, device="cpu", project="wasor_wasor",name="runs")
